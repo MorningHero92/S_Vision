4b641e1a7a46f3985427fb37dc3535d36ab787c5
MorningHero92 <131283074+MorningHero92@users.noreply.github.com>
2026-03-19 17:35:52 +0200

    Update tracker.py

--- src/traffic/tracking/tracker.py
+++ src/traffic/tracking/tracker.py
@@ -1,1 +1,9 @@
+# src/traffic/tracking/tracker.py
+class Tracker:
+    def __init__(self, tracker_yaml=None):
+        self.tracker_yaml = tracker_yaml
+        self.persist = True  # store track IDs
 
+    def track(self, frame, detector_results):
+        results = detector_results
+        return results
