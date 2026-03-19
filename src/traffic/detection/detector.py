3886c3266482bd6d5661227d1d80577018e8d1ee
MorningHero92 <131283074+MorningHero92@users.noreply.github.com>
2026-03-19 18:16:29 +0200

    Update detector.py

--- src/traffic/detection/detector.py
+++ src/traffic/detection/detector.py
@@ -5,6 +5,6 @@ class Detector:
     def __init__(self, model_path="yolo26m.pt"):
         self.model = YOLO(model_path)
 
-    def detect(self, frame, conf=0.25, classes=[2, 3, 5, 7])
+    def detect(self, frame, conf=0.25, classes=[2, 3, 5, 7]):
         results = self.model.predict(frame, conf=conf, classes=classes)
         return results
