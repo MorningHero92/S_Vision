4e716a1f6add5d2aebbf03f2fb5a9d9de70b8152
MorningHero92 <131283074+MorningHero92@users.noreply.github.com>
2026-03-20 20:02:32 +0200

    Update tracker.py

--- src/traffic/tracking/tracker.py
+++ src/traffic/tracking/tracker.py
@@ -35,7 +35,7 @@ def run_tracking(
             break
 
         results = model.track(
-            source=frame,
+            frame,
             tracker=tracker_yaml,
             persist=True
         )
