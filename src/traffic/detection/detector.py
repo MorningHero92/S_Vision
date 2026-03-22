be49edcc49266da362df6b2eae449852a289929f
MorningHero92 <131283074+MorningHero92@users.noreply.github.com>
2026-03-22 15:38:10 +0200

    Update detector.py

--- src/traffic/detection/detector.py
+++ src/traffic/detection/detector.py
@@ -1,1 +1,44 @@
-# reserved for future development
+# tracking.py
+
+import cv2
+from ultralytics import YOLO
+
+
+def run_prediction(
+    model_path,
+    video_path,
+    output_path,
+    tracker_yaml,
+    **kwargs
+):
+    model = YOLO(model_path)
+    cap = cv2.VideoCapture(video_path)
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    out = cv2.VideoWriter(
+        output_path,
+        fourcc,
+        fps,
+        (
+            width,
+            height,
+        ),
+    )
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        results = model.predict(
+            frame,
+            **kwargs
+        )
+
+        annotated_frame = results[0].plot()
+        out.write(annotated_frame)
+
+    cap.release()
+    out.release()
